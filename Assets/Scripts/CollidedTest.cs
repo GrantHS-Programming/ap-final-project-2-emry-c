6e253b05c60f2586325692860ce0918f30b8777e
emry <98835941+emry-c@users.noreply.github.com>
2023-06-04 22:21:00 -0700

    Testing getting tile position
    need to figure out how to keep position without converting to an int. gets truncated

--- Assets/Scripts/CollidedTest.cs
+++ Assets/Scripts/CollidedTest.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CollidedTest : MonoBehaviour
 {
     public int Num;
     public bool canInteract = false;
+
+    private Vector3Int tilePosition;
    
 void Update() 
    { 
-     if (Input.GetKeyDown(KeyCode.Keypad1))
+     if (canInteract && Input.GetKeyDown(KeyCode.Alpha1))
         {
             print("Key 1 pressed");
+            SetTileData();
         }
  }
 private void OnTriggerEnter2D(Collider2D person)
     {
        if (person.tag == "Player") 
         { 
-            print("collided");
+            print("collided" + Num);
             canInteract = true;
+            
            // public T GetTile(Vector3Int position);
             //print(position);
         }
@@ -32,10 +37,20 @@ private void OnTriggerEnter2D(Collider2D person)
             return;
         }
         print("can interact");
+        print(person.transform.position);
+        tilePosition = Vector3Int.CeilToInt(person.transform.position);
+        print(tilePosition);
     }
 
     private void OnTriggerExit2D(Collider2D person) {
         canInteract = false;
         print("Exit");
+    }
+
+    private void SetTileData(){
+        Tilemap tilemap = GetComponent<Tilemap>();
+        TileBase tile = tilemap.GetTile(tilePosition);
+        //.gameObject.GetInstanceID();
+        print(tile.GetInstanceID());
     }
 }
